9701bee40fa4deecb079c255a4a7a167a9541f77
Mark Sirany <marksirany@me.com>
2020-02-01 15:35:41 -0600

    help from tutor

--- employee-directory/src/App.js
+++ employee-directory/src/App.js
@@ -1,13 +1,87 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
-import Navbar from "./components/Navbar"
+//  import Header from "./components/"
+import List from "./components/List"
+import SearchForm from "./components/Form"
+import API from "./components/api"
+import axios from "axios"
+import Header from "./components/Header"
 
+class App extends Component {
+  state = {
+    users: [],
+    search: "",
+    usersFilter: []
 
-export default function App() {
+  }
 
-  return (
-    <div className="App">
-      <Navbar />
-    </div>
-  );
+  componentDidMount() {
+
+    const BASEURL = "https://randomuser.me/api/?results=200";
+
+    // Export an object with a "search" method that searches the RAMDOMUSER API for the passed query
+
+    axios.get(BASEURL)
+      .then(db => {
+
+        // call the api for get the users
+        const getusers = db.data.results
+        this.setState({ users: getusers, usersFilter: getusers })
+      })
+  }
+
+  handleInputChange = event => {
+    const name = event.target.name;
+    const value = event.target.value;
+    this.setState({
+      [name]: value,
+      usersFilter: this.state.users.filter(user => (user.name.first.toLowerCase()).indexOf(this.state.search.toLowerCase()) !== -1)
+    });
+  };
+
+  // When the form is submitted, search the  API for `this.state.search`
+  handleFormSubmit = event => {
+    event.preventDefault();
+    console.log("searchterm:", this.state.search)
+    this.setState({ usersFilter: this.state.users.filter(user => (user.name.first.toLowerCase()).indexOf(this.state.search.toLowerCase()) !== -1) })
+
+    // this.searchGiphy(this.state.search);
+  };
+
+  //sort array was taken from stackoverflow
+  sortAge() {
+    let sortedArray = this.state.users
+    function compare(a, b) {
+      if (a.dob.age > b.dob.age) {
+        return -1;
+      }
+      if (a.dob.age < b.dob.age) {
+        return 1;
+      }
+      // a must be equal to b
+      return 0;
+    }
+    this.setState({ user: sortedArray.sort(compare) })
+  }
+
+  // render will pull the information fom the components library 
+  render() {
+    return (
+      <div className="App">
+        <Header />
+        <SearchForm
+          search={this.state.search}
+          handleFormSubmit={this.handleFormSubmit}
+          handleInputChange={this.handleInputChange}
+        />
+        <button onClick={() => this.sortAge()}>Sort by Age</button>
+        <List
+          users={this.state.usersFilter}
+        />
+
+      </div>
+    )
+  }
 }
+
+export default App;
---
+++ employee-directory/src/components/List.js
@@ -0,0 +1,32 @@
+import React from "react";
+//render information
+function List(props) {
+
+    return (
+        <table className="table">
+            <thead>
+                <tr>
+                    <th scope="col">First</th>
+                    <th scope="col">Last</th>
+                    <th scope="col">Age</th>
+                    <th scope="col">City</th>
+                    <th scope="col">email</th>
+                </tr>
+            </thead>
+            <tbody>
+                {props.users.map(user =>
+                    <tr>
+                        <td >{user.name.first}</td>
+                        <td >{user.name.last}</td>
+                        <td >{user.dob.age}</td>
+                        <td >{user.location.city}</td>
+                        <td >{user.email}</td>
+                    </tr>
+                )}
+
+            </tbody>
+        </table>
+    )
+
+}
+export default List;
